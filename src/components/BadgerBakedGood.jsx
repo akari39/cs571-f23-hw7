f8a5f7eebc5c9e4a6b363bb305dbaf17943f189c
Akari_Hoshisaki <yizazaza@gmail.com>
2024-03-20 22:40:44 +0800

    Finish hw7.

--- src/components/BadgerBakedGood.jsx
+++ src/components/BadgerBakedGood.jsx
@@ -1,8 +1,40 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Text, View, Image, StyleSheet, Button } from "react-native";
 
 export default function BadgerBakedGood(props) {
-    
+
     return <View>
-        <Text>I am a baked good!</Text>
+        <View
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
+            <Image
+                style={{
+                    width: 200,
+                    height: 200,
+                }}
+                source={{
+                    uri: props.good.imgSrc,
+                }}
+            />
+            <Text
+                style={{
+                    fontSize: 24,
+                    fontWeight: 500,
+                }}>
+                {props.good.name}
+            </Text>
+            <Text
+                style={{
+                    marginTop: 12,
+                }}>
+                ${props.good.price}
+            </Text>
+            <Text>
+                You can order {props.good.upperLimit === -1 ? 'unlimited' : `up to ${props.good.upperLimit}`} units!
+            </Text>
+        </View>
     </View>
 }
--- src/components/BadgerBakery.jsx
+++ src/components/BadgerBakery.jsx
@@ -1,12 +1,141 @@
-import { Text, View } from "react-native";
+import { Alert, Button, Text, View } from "react-native";
 import BadgerBakedGood from "./BadgerBakedGood";
+import { useEffect, useState } from "react";
+import CS571 from '@cs571/mobile-client';
 
 export default function BadgerBakery() {
+    const [goods, setGoods] = useState({});
+    const [basket, setBasket] = useState({});
+    const [index, setIndex] = useState(0);
+    const [orderTotal, setOrderTotal] = useState(0.00);
+
+    async function fetchGoods() {
+        const goods = await fetch("https://cs571.org/api/f23/hw7/goods", {
+            headers: {
+                "X-CS571-ID": CS571.getBadgerId(),
+            }
+        })
+            .then(async res => {
+                const json = await res.json();
+                if (res.status === 200) {
+                    return json;
+                }
+                throw new Error(json.msg);
+            })
+            .catch(err => {
+                alert(err.message);
+            });
+        if (goods === undefined || goods === null) {
+            setGoods([]);
+            return;
+        }
+        setGoods(goods);
+    }
+
+    useEffect(() => fetchGoods(), []);
+
+    useEffect(() => {
+        calculatetotalPrice();
+    }, [basket]);
+
+    function onPrevious() {
+        setIndex(index - 1);
+    }
+
+    function onNext() {
+        setIndex(index + 1);
+    }
+
+    function onGoodChange(key, unit) {
+        var newBasket = JSON.parse(JSON.stringify(basket));
+        if (unit === 0) {
+            delete newBasket[key];
+            setBasket(newBasket);
+            return;
+        }
+        newBasket[key] = unit;
+        setBasket(newBasket);
+    }
+
+    function calculatetotalPrice() {
+        var totalPrice = 0;
+        for (let [key, value] of Object.entries(basket)) {
+            totalPrice += value * goods[key].price;
+        }
+        setOrderTotal(totalPrice);
+    }
+
+    const goodsKeys = Object.keys(goods);
+    if (goodsKeys.length === 0) {
+        return <></>;
+    }
+    const currentKey = goodsKeys[index];
+    const currentUnit = basket[currentKey] ?? 0;
+    const currentGood = goods[currentKey];
+
+    function submit() {
+        var itemCount = 0;
+        for (let [key, value] of Object.entries(basket)) {
+            itemCount += value;
+        }
+        Alert.alert("Order Confirmed!", `Your order contains ${itemCount} items and would have cost $${orderTotal.toFixed(2)}!`);
+    }
 
     return <View>
-        <Text>Welcome to Badger Bakery!</Text>
-        <BadgerBakedGood/>
-        <BadgerBakedGood/>
-        <BadgerBakedGood/>
+        <Text style={{
+            fontWeight: 500,
+        }}>Welcome to Badger Bakery!</Text>
+        <View style={{
+            marginTop: 10,
+            marginBottom: 20,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
+            <Button disabled={index <= 0} onPress={onPrevious} title="Previous" />
+            <View style={{ width: 4 }} />
+            <Button disabled={index >= goodsKeys.length - 1} onPress={onNext} title="Next" />
+        </View>
+        {
+            <BadgerBakedGood
+                good={currentGood}
+            />
+        }
+        <Text style={{
+            marginTop: 12,
+        }}>
+            Order Total: ${orderTotal.toFixed(2)}
+        </Text>
+        <View style={{
+            marginTop: 12,
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
+            <Button
+                title="-"
+                disabled={currentUnit <= 0}
+                onPress={() => onGoodChange(currentKey, currentUnit - 1)} />
+            <Text style={{
+                marginLeft: 8,
+                marginRight: 8,
+            }}>
+                {currentUnit ?? 0}
+            </Text>
+            <Button
+                title="+"
+                disabled={currentGood.upperLimit !== -1 && currentUnit >= currentGood.upperLimit}
+                onPress={() => onGoodChange(currentKey, currentUnit + 1)} />
+        </View>
+        <View style={{
+            marginTop: 12,
+        }}>
+            <Button
+                title="Place Order"
+                disabled={Object.keys(basket).length === 0}
+                onPress={submit} />
+        </View>
     </View>
 }
